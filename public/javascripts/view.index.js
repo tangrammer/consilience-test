9e29a1e159f7f28528bf6ef22b645aa20be29cf1
juanantonioruz <juanantonioruz@gmail.com>
2012-12-21 13:08:46 +0100

    integration javascript libs with client finished and working

--- public/javascripts/view.index.js
+++ public/javascripts/view.index.js
@@ -6,7 +6,7 @@ function prepare_main_div(){
     $(_main_content_div).empty();
 }
 function include_main_div(html){
-   $(_main_content_div).append(html);
+   $(_main_content_div).append(html+"<br>");
 }
 
 function load_person(id){
@@ -39,3 +39,33 @@ function start(){
     include_main_div("<h1>Select one option from the side bar</h1>");
 
 }
+
+function print_person(p){
+    prepare_main_div();
+    include_main_div("FNAME : "+p.get_fname());
+    include_main_div("WAGE : "+p.get_wage());
+    include_main_div("DOB : "+p.get_DOB());
+}
+
+function basic_person(){
+    
+    this.person=create_person(data_person_example);
+    print_person(this.person);
+    
+}
+
+
+
+function i18n_person(){
+    
+    this.person=internationalize(create_person(data_person_example));
+    print_person(this.person);
+    
+    
+}
+function localize(lang_){
+set_locale(lang_);
+    print_person(this.person);
+}
+
+
